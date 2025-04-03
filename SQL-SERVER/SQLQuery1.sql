fb214702ea8824c63df5a18f5d4603805f5930e7
Sudhanshu-GIT-00 <amanraja.in.in@gmail.com>
2025-04-03 09:20:58 +0530

    we create the new file of sql

--- SQL-SERVER/SQLQuery1.sql
+++ SQL-SERVER/SQLQuery1.sql
@@ -1,15 +1,15 @@
-create database myyfile;
-use myyfile
+-- create database myyfile;
+-- use myyfile
 
-create table employe(
-custm_id int primary key, 
-custm_name varchar(20), 
-m_no int, 
-address varchar(50),
-city varchar (20), 
-postal_code int,
-countery varchar(20));
+-- create table employe(
+-- custm_id int primary key, 
+-- custm_name varchar(20), 
+-- m_no int, 
+-- address varchar(50),
+-- city varchar (20), 
+-- postal_code int,
+-- countery varchar(20));
 
 
-insert into employe values(1,'jai',987464123,'bihar buxar','buxar',802101,'india');
-select * from employe
+-- insert into employe values(1,'jai',987464123,'bihar buxar','buxar',802101,'india');
+-- select * from employe
