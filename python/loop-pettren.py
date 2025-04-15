22f574bb5ab35b09f25f19f60843c5f1cb970f39
root <root@raktbeej>
2025-04-15 18:23:16 +0530

    i am created a new file of python and some loop program

---
+++ python/loop-pettren.py
@@ -0,0 +1,32 @@
+
+# -- f string uae for concatenation, f is function 
+# f"{i},{j}"
+# x= int(input("Enter your name: "))
+# y= int(input("Enter your name: "))
+# for i in range(x,y):
+#     for j in range(x,i+1):
+#         print("*", end=" ")
+#     print("\n")
+
+#  x=int(input("enter no : "))
+#  y=int(input("enter no : "))
+#  for i in range(x,y):
+#      for j in range(x,i+1):
+#          print("*",end=" ")
+#      print("\n")
+
+#  x=int(input("enter no : "))
+#  y=int(input("enter no : "))
+#  while x<y:
+#      print(i, end=" ")
+#      x+=1
+#  print("\n")
+
+x=int(input("enter no : "))
+y=int(input("enter no : "))
+for i in range(x,y):
+    j=0
+    while x<y and j<i:
+        j+=1
+        print("*", end=" ")
+    print("\n")
